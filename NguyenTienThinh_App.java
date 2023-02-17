7c0d6df511104e2d834355f5e40defab374ca966
Ntthinh192003 <125591207+Ntthinh192003@users.noreply.github.com>
2023-02-17 08:43:06 +0700

    Add files via upload

---
+++ NguyenTienThinh_App.java
@@ -0,0 +1,9 @@
+/**
+ * NguyenTienThinh_App
+ */
+public class NguyenTienThinh_App {
+
+    public static void main(String[] args) {
+        System.out.println("Nguyen Tien Thinh hoc Java");
+    }
+}
